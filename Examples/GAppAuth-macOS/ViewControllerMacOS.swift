756cddb24e8d41de18259c4b355099a3d9ba0dfc
Jonas-Taha El Sesiy <elsesiy@gmail.com>
2018-12-21 02:34:58 +0100

    Add Examples for macOS and iOS

---
+++ Examples/GAppAuth-macOS/ViewControllerMacOS.swift
@@ -0,0 +1,69 @@
+//
+//  ViewControllerMacOS.swift
+//  GAppAuth-macOS
+//
+//  Created by Jonas-Taha El Sesiy on 07.03.18.
+//  Copyright © 2018 Jonas-Taha El Sesiy. All rights reserved.
+//
+
+import Cocoa
+import GAppAuth
+
+class ViewControllerMacOS: NSViewController {
+    
+    @IBOutlet weak var accessToken: NSTextField!
+    @IBOutlet weak var refreshToken: NSTextField!
+    @IBOutlet weak var scopes: NSTextField!
+    @IBOutlet weak var email: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        GAppAuth.shared.retrieveExistingAuthorizationState()
+        
+        if GAppAuth.shared.isAuthorized() {
+            let authorization = GAppAuth.shared.getCurrentAuthorization()
+            self.updateUI(authorization)
+        }
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            if GAppAuth.shared.isAuthorized() {
+                let authorization = GAppAuth.shared.getCurrentAuthorization()
+                self.updateUI(authorization)
+            }
+        }
+    }
+    
+    @IBAction func click(_ sender: NSButton?) {
+        do {
+            try GAppAuth.shared.authorize { auth in
+                if auth {
+                    if GAppAuth.shared.isAuthorized() {
+                        let authorization = GAppAuth.shared.getCurrentAuthorization()
+                        self.updateUI(authorization)
+                    }
+                }
+            }
+        } catch let error {
+            print(error.localizedDescription)
+            updateUI(nil)
+        }
+        
+    }
+    
+    @IBAction func logout(_ sender: NSButton?) {
+        GAppAuth.shared.resetAuthorizationState()
+        updateUI(nil)
+    }
+    
+    private func updateUI(_ authorization: GTMAppAuthFetcherAuthorization?) {
+        let tokenResponse = authorization?.authState.lastTokenResponse
+        
+        self.accessToken.stringValue = tokenResponse?.accessToken ?? ""
+        self.refreshToken.stringValue = tokenResponse?.refreshToken ?? ""
+        self.scopes.stringValue = tokenResponse?.scope ?? ""
+        self.email.stringValue = authorization?.userEmail ?? ""
+    }
+}
